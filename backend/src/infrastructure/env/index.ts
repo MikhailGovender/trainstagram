545111a6db5fc275f34beb265a2533923bd5fdc5
willa <willa@indiefin.com>
2023-07-27 11:49:10 +0200

    fix: add missing env vars lost after merge from main

--- backend/src/infrastructure/env/index.ts
+++ backend/src/infrastructure/env/index.ts
@@ -4,6 +4,8 @@ config();
 
 export const ENV = {
   TEST_VAR: process.env.TEST_VAR,
+  PORT: process.env.PORT || "8080",
+  SESSION_SECRET: process.env.SESSION_SECRET || "",
   DB_SERVER: process.env.DB_SERVER || "localhost",
   DB_NAME: process.env.DB_NAME || "Trainstagram",
   DB_USER: process.env.DB_USER || "root",
